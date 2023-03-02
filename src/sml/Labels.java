320cf5c9e886433678728e1da9ae360605bc2989
Ken <ken@Kens-MacBook-Pro-2.local>
2023-03-02 01:09:24 +0000

    completed TODOs

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -1,14 +1,18 @@
 package sml;
 
+import sml.instruction.AddInstruction;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+
+import java.util.stream.Collectors;
 
 // TODO: write a JavaDoc for the class
 
 /**
  *
- * @author ...
+ * @author kmoriw01
  */
 public final class Labels {
 	private final Map<String, Integer> labels = new HashMap<>();
@@ -21,8 +25,12 @@ public final class Labels {
 	 */
 	public void addLabel(String label, int address) {
 		Objects.requireNonNull(label);
-		// TODO: Add a check that there are no label duplicates.
-		labels.put(label, address);
+		// TODO: Add a check that there are no label duplicates. -- done
+		if (labels.containsKey(label)) {
+			System.out.println(label + " is duplicated!");
+		} else {
+			labels.put(label, address);
+		}
 	}
 
 	/**
@@ -34,8 +42,19 @@ public final class Labels {
 	public int getAddress(String label) {
 		// TODO: Where can NullPointerException be thrown here?
 		//       (Write an explanation.)
-		//       Add code to deal with non-existent labels.
-		return labels.get(label);
+		//       Add code to deal with non-existent labels. -- done
+		// Explanation: it can be thrown here
+		// where there is no label argument.
+		// We can re-write the code with try/catch blocks.
+		// If the label exist, return NORMAL_PROGRAM_COUNTER_UPDATE
+		// value, which is -1.
+
+		try {
+			return labels.get(label);
+		} catch(NullPointerException e) {
+			System.out.println(label + " does not exist!");
+		}
+		return -1;
 	}
 
 	/**
@@ -46,11 +65,32 @@ public final class Labels {
 	 */
 	@Override
 	public String toString() {
-		// TODO: Implement the method using the Stream API (see also class Registers).
-		return "";
+		// TODO: Implement the method using the Stream API (see also class Registers). -- done
+
+		return labels.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.map(e -> e.getKey() + " = " + e.getValue())
+				.collect(Collectors.joining(", ", "[", "]")) ;
 	}
 
 	// TODO: Implement equals and hashCode (needed in class Machine).
+
+	@Override
+	public boolean equals(Object o){
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()){
+			return false;
+		}
+		Labels that = (Labels) o;
+		return labels.equals(that.labels);
+	}
+	@Override
+	public int hashCode(){
+		return Objects.hash(labels);
+	}
+
 
 	/**
 	 * Removes the labels
