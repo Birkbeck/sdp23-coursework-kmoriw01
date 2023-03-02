0a1abdcac9603736fe6783033d1136cc79648b83
Ken <ken@Kens-MacBook-Pro-2.local>
2023-03-02 18:38:15 +0000

    completed TODO

--- src/sml/Registers.java
+++ src/sml/Registers.java
@@ -3,11 +3,11 @@ package sml;
 import java.util.*;
 import java.util.stream.Collectors;
 
-// TODO: write a JavaDoc for the class
+// TODO: write a JavaDoc for the class -- done
 
-/**
+/** This represents Registers class.
  *
- * @author ...
+ * @author kmoriw01
  */
 public final class Registers {
     private final Map<Register, Integer> registers = new HashMap<>();
@@ -45,12 +45,11 @@ public final class Registers {
         return registers.get((Register)register);
     }
 
-    // TODO: use pattern matching for instanceof
+    // TODO: use pattern matching for instanceof -- done
     // https://docs.oracle.com/en/java/javase/14/language/pattern-matching-instanceof-operator.html
     @Override
     public boolean equals(Object o) {
-        if (o instanceof Registers) {
-            Registers other = (Registers) o;
+        if (o instanceof Registers other) {
             return registers.equals(other.registers);
         }
         return false;
